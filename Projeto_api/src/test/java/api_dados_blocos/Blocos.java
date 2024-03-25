47a478e1d5228edde053d0450a00bd2aeae9c8fd
BetoSam_git <jsbsam_lb@hotmail.com>
2024-03-25 13:58:10 -0300

    enpoint global

--- Projeto_api/src/test/java/api_dados_blocos/Blocos.java
+++ Projeto_api/src/test/java/api_dados_blocos/Blocos.java
@@ -9,12 +9,12 @@ import io.restassured.response.Response;
 
 public class Blocos {
 	int code;
-
+	Response response = RestAssured
+			.get("https://dadosabertos.camara.leg.br/api/v2/blocos?ordem=ASC&ordenarPor=nome");
 	@Test
 
 	public void testPesquisarBlocos() {
-		Response response = RestAssured
-				.get("https://dadosabertos.camara.leg.br/api/v2/deputados?ordem=ASC&ordenarPor=nome");
+	
 
 		code = response.getStatusCode();
 
@@ -26,9 +26,7 @@ public class Blocos {
 	@Test
 	public void validarId() {
 		int posicao = 0;
-		Response response = RestAssured
-
-				.get("https://dadosabertos.camara.leg.br/api/v2/blocos?ordem=ASC&ordenarPor=nome");
+		
 
 		String id = response.jsonPath().getString("dados.id[" + posicao + "]".toString());
 
@@ -41,8 +39,7 @@ public class Blocos {
 
 		String nomeEsperado = "Federação Brasil da Esperança - Fe Brasil";
 		int posicao = 0;
-		Response response = RestAssured
-				.get("https://dadosabertos.camara.leg.br/api/v2/blocos?ordem=ASC&ordenarPor=nome");
+		
 
 		String nome = response.jsonPath().getString("dados.nome[" + posicao + "]").toString();
 		assertEquals(nome, nomeEsperado);
@@ -55,9 +52,7 @@ public class Blocos {
 
 		String idLegislacaoEsperada = "57";
 		int posicao = 0;
-		Response response = RestAssured
-				.get("https://dadosabertos.camara.leg.br/api/v2/blocos?ordem=ASC&ordenarPor=nome");
-
+		
 		String idLegislacao = response.jsonPath().getString("dados.idLegislatura[" + posicao + "]").toString();
 		assertEquals(idLegislacao, idLegislacaoEsperada);
 		System.out.println(" IdLesgislacao retornada  : " + idLegislacao);
